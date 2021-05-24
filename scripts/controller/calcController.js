fda4a8daed238e54ca033ac9e3e5f771fc927fce
henriquesabarah <henriquesabarah@gmail.com>
2021-05-24 17:16:23 -0300

    usando dom

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -5,7 +5,13 @@ class CalcController {
         this.initialize();
     }
     initialize() {
+        let displayCalcEl = document.querySelector('#display');
+        let dateEl = document.querySelector('#data');
+        let timeEl = document.querySelector('#hora');
 
+        displayCalcEl.innerHTML = "4567";
+        dateEl.innerHTML = "01/01/2021";
+        timeEl.innerHTML = "00:00";
     }
     get displayCalc() {
         return this._displayCalc;
